b16d5037057b0e0b40b3f8af9090f46ac23c7ea0
alfre <alibarra@csumb.edu>
2023-02-24 01:03:48 -0800

    Progress So far on StudentDTO. Look up Auto increment/Generated value

--- cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
@@ -1,5 +1,35 @@
 package com.cst438.domain;
 
+import java.util.Objects;
+
 public class StudentDTO {
+	public int student_id;
+	public String name;
+	public String email;
+	public String status; 
+	public int statusCode;
+
+	
+	@Override
+	public String toString() {
+		return "StudentDTO {student_id=" + student_id + ", name=" + name + ", email=" + email + ", status=" + status
+				+ ", statusCode=" + statusCode + "}";
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StudentDTO other = (StudentDTO) obj;
+		return Objects.equals(email, other.email) && Objects.equals(name, other.name)
+				&& Objects.equals(status, other.status) && statusCode == other.statusCode
+				&& student_id == other.student_id;
+	}
+	
 
 }
